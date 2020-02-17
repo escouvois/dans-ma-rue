e45187a452679eae0ffee100addd0c7c899cefc7
Bastien ESCOUVOIS <bastien.escouvois@gmail.com>
2020-02-17 16:37:47 +0100

    countAround

--- services/count.js
+++ services/count.js
@@ -2,7 +2,6 @@ const config = require('config');
 const indexName = config.get('elasticsearch.index_name');
 
 exports.count = (client, from, to, callback) => {
-    
     client
     .count({
       index: indexName,
@@ -22,13 +21,31 @@ exports.count = (client, from, to, callback) => {
             count: resp.body.count
         })
     });
-
-    
 }
 
 exports.countAround = (client, lat, lon, radius, callback) => {
     // TODO Compter le nombre d'anomalies autour d'un point géographique, dans un rayon donné
-    callback({
-        count: 0
+    client.count({
+        index: indexName,
+        body: {
+            query: {
+                bool: {
+                    must: {
+                        match_all: {}
+                    },
+                    filter: {
+                        geo_distance: {
+                            distance: radius,
+                            "anomalie.location": lat + ',' + lon
+                        }
+                    }
+                }
+            }
+        }
+    }).then(resp => {
+        callback({
+            count: resp.body.count
+        })
     })
+    
 }
