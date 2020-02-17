6da8c2984e789ff897ea1fdbd868bd30afa82fd3
Bastien ESCOUVOIS <bastien.escouvois@gmail.com>
2020-02-17 18:36:37 +0100

    some stats

--- services/stats.js
+++ services/stats.js
@@ -12,11 +12,47 @@ exports.statsByType = (client, callback) => {
 }
 
 exports.statsByMonth = (client, callback) => {
-    // TODO Trouver le top 10 des mois avec le plus d'anomalies
-    callback([]);
+    // Trouver le top 10 des mois avec le plus d'anomalies
+    // TODO - Aggréger mois/année dans la key dans le résultat
+    client.search({
+        index: indexName,
+        body: {
+            size: 0,
+            aggs: {
+                "mois": {
+                    terms: {
+                        field: "anomalie.mois_declaration.keyword",
+                        size: 10
+                    }
+                }
+            }
+        }
+    }).then(resp => {
+        callback(resp.body.aggregations.mois.buckets)
+    })
 }
 
 exports.statsPropreteByArrondissement = (client, callback) => {
-    // TODO Trouver le top 3 des arrondissements avec le plus d'anomalies concernant la propreté
-    callback([]);
+    // Trouver le top 3 des arrondissements avec le plus d'anomalies concernant la propreté
+    client.search({
+        index: indexName,
+        body: {
+            size: 0,
+            aggs: {
+                "arrondissementsByType": {
+                    filter: { term: { "anomalie.type.keyword": "Propreté" }},
+                    aggs: {
+                        "arrondissements": {
+                            terms: {
+                                field: "anomalie.arrondissement.keyword",
+                                size: 3
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }).then(resp => {
+        callback(resp.body.aggregations.arrondissementsByType.arrondissements.buckets)
+    })
 }
